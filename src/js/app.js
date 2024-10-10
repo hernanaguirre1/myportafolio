e6b809d58ef94cb5bed271669a0ac2f298b9a9c3
hernanaguirre1 <hernanaguirre3012@gmail.com>
2024-10-10 18:03:04 -0300

    principal terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,67 @@
-const cursor = document.querySelector('.cursor')
+const header = document.querySelector('header')
+const explorar = document.querySelector('.explorar')
+const principal= document.querySelector('.principal')
+const inicio = document.querySelector('.inicio')
+const botones = document.querySelectorAll('.boton')
 
-document.addEventListener('mousemove',(e)=>{
-    let x = e.clientX - 17;
-    let y = e.clientY - 17;
-    cursor.style.left = `${x}px`;
-    cursor.style.top = `${y}px`;
-    cursor.style.display = 'block';
-})
-document.addEventListener('mouseout',()=>{
-    cursor.style.display = "none"
-})
-
-
-//  PROYECTOS JS 
-
-const main = document.querySelector('.main-proyectos')
-const botones = document.querySelectorAll('.proyecto')
-const contenedor = document.querySelector('.contenedor')
-
-botones.forEach ((boton)=> {
-    boton.addEventListener('click', ()=> {
-       const div = document.createElement('DIV')
-        div.classList.add('conteiner')
-        const h2 = document.createElement('H2')
-        const img = document.createElement('IMG')
-        const text = document.createElement('P')
-        const vovler = document.createElement('BUTTON')
-       const a = document.createElement('A')
-       a.classList.add('ver-pagina')
-        vovler.classList.add('volver')
-        main.appendChild(div)
-        const conteiner = document.querySelector('.conteiner')
-        main.appendChild(vovler)
-        a.textContent = 'ver página'
-        a.target = '_blank'
-        vovler.textContent = 'volver'
-        vovler.addEventListener('click', ()=> {
-            contenedor.style.display = 'flex'
-            main.removeChild(div)
-            main.removeChild(vovler)
+botones[0].classList.add('select')
+  
+  explorar.addEventListener('click' , ()=> {
+    header.classList.add('active')
+    principal.style.animation = 'salida .3s ease-in'
+    setTimeout ( ()=> {
+        principal.style.display = 'none'
+    }, 300)
+  })
+  
+  botones.forEach( boton => {
+        boton.addEventListener('click' , ()=> {
+            if (boton.classList.contains('inicio')) {
+                
+            }
         })
-        if(boton.classList.contains('festival')) {
-            contenedor.style.display = 'none'
-            text.textContent = 'Un proyecto realizado con SASS para un festival de musica ficticio, el diseño totalmente responsivo permite que los usuarios puedan navegar desde cualquier dispositivo'
-            img.src = 'src/img/proyecto-festival.png'
-            img.alt = 'proyecto'
-            h2.textContent = 'Festival de musica 2024'
-            a.href = 'https://festivaldemontevideomusic.netlify.app'
-            conteiner.appendChild(h2)
-            conteiner.appendChild(img)
-            conteiner.appendChild(text)
-            conteiner.appendChild(a)
+  })
+  
+  
+  
+  
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
-    })
-})
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.1,
+        "random": true
+      },
+      "size": {
+        "value": 10,
+        "random": true
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "bottom",
+        "straight": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      }
+    }
+  });
