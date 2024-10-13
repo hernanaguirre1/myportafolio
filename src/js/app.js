533c85c8beb92c477f1fbd215ec1d3a8d06db741
hernanaguirre1 <hernanaguirre3012@gmail.com>
2024-10-13 17:56:35 -0300

    terminado

--- src/js/app.js
+++ src/js/app.js
@@ -44,6 +44,10 @@ particlesJS("particles-js", {
   const botones = document.querySelectorAll('.boton')
   const principal = document.querySelector('#principal')
   const contacto = document.querySelector('#contacto')
+  const proyectos = document.querySelector('#proyectos')
+  const proyectosBtn = document.querySelectorAll('.proyecto')
+  const festival = document.querySelector('.festival')
+  const navegacion = document.querySelector('.botones')
 
   botones[0].classList.add('active')
 
@@ -54,14 +58,39 @@ particlesJS("particles-js", {
 
       if(boton.classList.contains('contacto')) {
           principal.style.display = 'none'
+          proyectos.style.display = 'none'
           contacto.style.animation = 'entrada .2s ease-in'
         contacto.style.display = 'flex'
       } else if(boton.classList.contains('inicio')) {
          contacto.style.display = 'none'
+         proyectos.style.display = 'none'
           principal.style.display = 'flex'
           principal.style.animation = 'entrada .2s ease-in'
+      } else if (boton.classList.contains('proyectos')) {
+          contacto.style.display = 'none'
+          principal.style.display = 'none'
+          proyectos.style.display = 'flex'
+          proyectos.style.animation = 'entrada .2s ease-in'
       }
 
 
     })
+  })
+
+  proyectosBtn.forEach (btn => {
+    btn.addEventListener('click', ()=> {
+      if (btn.classList.contains('festival-proyecto')) {
+        festival.style.display = 'flex'
+        festival.style.animation = 'slide-in-elliptic-bottom-fwd 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both'
+      }
+    })
+  })
+
+  window.addEventListener('scroll', ()=> {
+    if (window.scrollY > 80) {
+      navegacion.style.top = '0'
+    }
+    else {
+      navegacion.style.top = '75px'
+    }
   })
